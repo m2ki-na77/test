aec86304deb0880783fe811c0cd3db5beb9771de
m2ki-na77 <daofighter@gmail.com>
2018-01-17 15:44:03 +0900

    ７まい

--- www/slick/ncmb.js
+++ www/slick/ncmb.js
@@ -29,6 +29,22 @@
                   var image_name3 = results[num3].get("image");
                   var image_cap3 = results[num3].get("caption");
                   var image_title3 = results[num3].get("name"); 
+                  var num4 = num - Math.floor(Math.random()*num);
+                  var image_name4 = results[num4].get("image");
+                  var image_cap4 = results[num4].get("caption");
+                  var image_title4 = results[num4].get("name"); 
+                  var num5 = num - Math.floor(Math.random()*num);
+                  var image_name5 = results[num5].get("image");
+                  var image_cap5 = results[num5].get("caption");
+                  var image_title5 = results[num5].get("name"); 
+                  var num6 = num - Math.floor(Math.random()*num);
+                  var image_name6 = results[num6].get("image");
+                  var image_cap6 = results[num6].get("caption");
+                  var image_title6 = results[num6].get("name"); 
+                  var num7 = num - Math.floor(Math.random()*num);
+                  var image_name7 = results[num7].get("image");
+                  var image_cap7 = results[num7].get("caption");
+                  var image_title7 = results[num7].get("name"); 
                   
                   
                   // Change the css styles and srcs according to the omiukji states  
@@ -36,7 +52,11 @@
                   $(".img1").attr("src",commonURL + image_name);
                   $(".img2").attr("src",commonURL + image_name2);
                   $(".img3").attr("src",commonURL + image_name3);
-                  $(".modalMain").html("<h4>"+image_title+"</h4>"+image_cap+"</br><h4>"+image_title2+"</h4>"+image_cap2+"</br><h4>"+image_title3+"</h4>"+image_cap3+"</br>");
+                  $(".img4").attr("src",commonURL + image_name4);
+                  $(".img5").attr("src",commonURL + image_name5);
+                  $(".img6").attr("src",commonURL + image_name6);
+                  $(".img7").attr("src",commonURL + image_name7);
+                  $(".modalMain").html("<h4>"+image_title+"</h4>"+image_cap+"</br><h4>"+image_title2+"</h4>"+image_cap2+"</br><h4>"+image_title3+"</h4>"+image_cap3+"</br><h4>"+image_title4+"</h4>"+image_cap4+"</br><h4>"+image_title5+"</h4>"+image_cap5+"</br><h4>"+image_title6+"</h4>"+image_cap6+"</br><h4>"+image_title7+"</h4>"+image_cap7+"</br>");
 
 
               },
